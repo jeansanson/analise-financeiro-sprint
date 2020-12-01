21c9e57a0b49d9d73b77942f36cd0a9419067aec
Bruno Klehm <brunowuklehm@gmail.com>
2020-12-01 20:26:20 -0300

    melhorado a responsividade da tela de mercados

--- financial-project/src/components/Mercados.vue
+++ financial-project/src/components/Mercados.vue
@@ -1,22 +1,50 @@
 <template>
   <v-container>
-    <h2 class="text-center mt-2 mb-2">Índices de bolsas de valores pelo mundo</h2>
+    <h2 class="text-center mt-2 mb-2">
+      Índices de bolsas de valores pelo mundo
+    </h2>
     <v-row dense>
-      <v-col v-for="stock in stocks" :key="stock.title" :cols="3">
+      <v-col
+        v-for="stock in stocks"
+        :key="stock.title"
+        :cols="cols"
+        class="text-center"
+      >
         <v-card>
-          <v-card-title v-text="stock.title" class="justify-center"></v-card-title>
-          <v-card-title v-text="stock.location" class="justify-center"></v-card-title>
-          <v-card-title v-text="stock.value + '%'" class="justify-center"></v-card-title>
+          <v-card-title
+            v-text="stock.title"
+            class="justify-center"
+          ></v-card-title>
+          <v-card-title
+            v-text="stock.location"
+            class="justify-center"
+            style="word-break: keep-all"
+          ></v-card-title>
+          <v-card-title
+            v-text="stock.value + '%'"
+            class="justify-center"
+          ></v-card-title>
         </v-card>
       </v-col>
     </v-row>
-    <h2 class="text-center mt-2 mb-2">Cotação das principais moedas para o real</h2>
+    <h2 class="text-center mt-2 mb-2">
+      Cotação das principais moedas para o real
+    </h2>
     <v-row dense>
-      <v-col v-for="currency in currencies" :key="currency.title" :cols="3">
+      <v-col v-for="currency in currencies" :key="currency.title" :cols="cols">
         <v-card>
-          <v-card-title v-text="currency.title + '/BRL'" class="justify-center"></v-card-title>
-          <v-card-title v-text="'R$' + currency.value" class="justify-center"></v-card-title>
-          <v-card-title v-text="currency.variation + '%'" class="justify-center"></v-card-title>
+          <v-card-title
+            v-text="currency.title + '/BRL'"
+            class="justify-center"
+          ></v-card-title>
+          <v-card-title
+            v-text="'R$' + currency.value"
+            class="justify-center"
+          ></v-card-title>
+          <v-card-title
+            v-text="currency.variation + '%'"
+            class="justify-center"
+          ></v-card-title>
         </v-card>
       </v-col>
     </v-row>
@@ -33,13 +61,31 @@ export default {
     stocks: [],
     currencies: [],
   }),
+  computed: {
+    cols() {
+      switch (this.$vuetify.breakpoint.name) {
+        case "xs":
+          return 6;
+        case "sm":
+          return 6;
+        case "md":
+          return 4;
+        case "lg":
+          return 3;
+        case "xl":
+          return 3;
+        default:
+          return 3;
+      }
+    },
+  },
   created() {
     axios.get("https://api.hgbrasil.com/finance").then((resp) => {
       const stockEntries = Object.keys(resp.data.results.stocks);
       const currenciesEntries = Object.keys(resp.data.results.currencies);
       const stocks = [];
       const currencies = [];
-      console.log(resp.data.results);
+
       stockEntries.forEach((element) => {
         const stock = resp.data.results.stocks[element];
         stocks.push({
@@ -49,19 +95,20 @@ export default {
         });
       });
       this.stocks = stocks;
+      
       currenciesEntries.forEach((element) => {
         const currency = resp.data.results.currencies[element];
-        if(element !== "source"){
+        if (element !== "source") {
           currencies.push({
             title: element,
-            value: String(currency.buy).replace(".",","),
+            value: String(currency.buy).replace(".", ","),
             variation: currency.variation,
           });
         }
       });
       this.currencies = currencies;
     });
-  }
+  },
 };
 </script>
 
